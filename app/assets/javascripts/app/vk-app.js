72b692a36e207c4b49fcbd070db896841a60be3a
yurijmi <me@yurijmi.ru>
2013-12-13 01:11:10 +0400

    Reverting changes

--- app/assets/javascripts/app/vk-app.js
+++ app/assets/javascripts/app/vk-app.js
@@ -1,23 +1,5 @@
 $(document).ready(function(){
 
-    var api = new vk_api(
 
-        'SRplcxaXW9o1C8dWUKjf',
-
-        function() {
-
-            api.makeSettings(api.SETT_AUDIO || api.SETT_STATUS);
-
-        }
-
-    );
-
-    $('#change_settings').click(function(e){
-
-        api.makeSettings(api.SETT_AUDIO || api.SETT_STATUS);
-
-        e.preventDefault();
-
-    });
 
 });
