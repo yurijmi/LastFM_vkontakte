9869860d8ffc37e8b1b1c92833f61e65f0c536da
yurijmi <me@yurijmi.ru>
2013-12-12 15:43:09 +0400

    This isn't working :(

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
-
-    I18n.locale = 'ru' if I18n.locale == 'ru_RU'
-    I18n.locale = 'en' if I18n.locale == 'en_US' || I18n.locale = 'en_GB'
   end
 
   def default_url_options(options = {})
