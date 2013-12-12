d586d0a61799a64577a591ed7e68e8ce84d27d72
yurijmi <me@yurijmi.ru>
2013-12-12 15:36:51 +0400

    Updated locale detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  require '1.9.1/rubygems/request'
-
   before_filter :set_locale
 
   def set_locale
     I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
+
+    I18n.locale = 'ru' if I18n.locale == 'ru_RU'
+    I18n.locale = 'en' if I18n.locale == 'en_US' || I18n.locale = 'en_GB'
   end
 
   def default_url_options(options = {})
